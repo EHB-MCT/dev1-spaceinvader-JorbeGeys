afa3ee489818cd91cf320d8cf451dc120a947f97
Jorbe Geys <jorbe.geys@student.ehb.be>
2022-12-05 10:25:19 +0100

    TAAK KLAAR Jorbe Geys

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,32 +1,30 @@
 "use strict";
 
-let canvas = document.querySelector('canvas');
+let canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
-let context = canvas.getContext('2d');
+let context = canvas.getContext("2d");
 
-drawSpaceInvader ();
+drawSpaceInvader();
 
-function drawSpaceInvader (){
-    
-    //zwart vierkant tekenen als achtergrond
-    context.fillStyle = "black";   
-    
-    context.beginPath();
-    
-    context.fillRect(50,50,300,300);
-    
-    context.stroke();
+function drawSpaceInvader() {
+	//zwart vierkant tekenen als achtergrond
+	context.fillStyle = "black";
 
+	context.beginPath();
 
-    //hexcode van internet gehaald
-    //eerst de bovenste balk tekenen en daarna het kleine vierkantje
-    context.fillStyle = "#AAFF00";
+	context.fillRect(50, 50, 300, 300);
 
-    context.beginPath();
+	context.stroke();
 
-    context.fillRect(75,100,250,50);
-    context.fillRect(175,150,50,50);
+	//hexcode van internet gehaald
+	//eerst de bovenste balk tekenen en daarna het kleine vierkantje
+	context.fillStyle = "#AAFF00";
 
-    context.stroke();
+	context.beginPath();
+
+	context.fillRect(75, 100, 250, 50);
+	context.fillRect(175, 150, 50, 50);
+
+	context.stroke();
 }
